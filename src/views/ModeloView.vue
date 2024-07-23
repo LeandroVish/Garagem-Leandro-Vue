0ee9acebc64b67b182da0052b5a2c89e489a6571
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-07-23 14:40:09 -0300

    modelos

--- src/views/ModeloView.vue
+++ src/views/ModeloView.vue
@@ -1,14 +1,23 @@
 <script setup>
 import { ref, reactive, onMounted } from "vue";
 import ModelosApi from "@/api/modelo"
-const modelosApi = new ModelosApi();
+import MarcasApi from "../api/marca"
+import CategoriasApi from "../api/categorias"
 
-const defaultmodelo = { id: null, nome: "" };
+const modelosApi = new ModelosApi();
+const categoriasApi = new CategoriasApi();
+const marcasApi = new MarcasApi();
+
+const defaultmodelo = { id: null, nome: "", marca: "", categoria: ""};
 const modelos = ref([]);
+const marcas = ref([]);
+const categorias = ref([]);
 const modelo = reactive({ ...defaultmodelo });
 
 onMounted(async () => {
   modelos.value = await modelosApi.buscarTodosOsModelo();
+  marcas.value = await marcasApi.buscarTodasAsMarca();
+  categorias.value = await categoriasApi.buscarTodasAsCategorias();
 });
 
 function limpar() {
@@ -41,6 +50,16 @@ async function excluir(id) {
   <hr />
   <div class="form">
     <input type="text" v-model="modelo.nome" placeholder="Nome" />
+    <select v-model="modelo.marca" name="marcas" id="marcas">
+      <option :value="marca.id" v-for="marca in marcas" :key="marca.id">
+        {{ marca.nome }}     
+      </option>
+    </select>
+    <select v-model="modelo.categoria" name="categorias" id="categorias">
+      <option :value="categoria.id" v-for="categoria in  categorias" :key="categoria.id">
+        {{ categoria.descricao }}
+      </option>
+    </select>
     <button @click="salvar">Salvar</button>
     <button @click="limpar">Limpar</button>
   </div>
@@ -48,7 +67,7 @@ async function excluir(id) {
   <ul>
     <li v-for="modelo in modelos" :key="modelo.id">
       <span @click="editar(modelo)">
-        ({{ modelo.id }}) - {{ modelo.nome }} -
+        ({{ modelo.id }}) - {{ modelo.nome }} - {{ modelo.marca }} - {{ modelo.categoria }}
       </span>
       <button @click="excluir(modelo.id)">X</button>
     </li>
